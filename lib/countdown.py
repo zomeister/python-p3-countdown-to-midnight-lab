fadb68eaa528dfd6525c25e5487efd37d294efd1
Zoe Guidry <oyezoey@gmail.com>
2023-05-26 13:27:31 -0500

    lab completed.

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
-# your code goes here!
+import time
+def countdown(int_sec):
+    while int_sec >= 0:
+        if int_sec == 0:
+            print("HAPPY NEW YEAR!")
+            break
+        print(f'{int_sec} SECOND(S)!')
+        int_sec -= 1
+def countdown_with_sleep(int_sec):
+    time.sleep(1)
+    while int_sec >= 0:
+        if int_sec == 0:
+            print("HAPPY NEW YEAR!")
+            break
+        print(f'{int_sec} SECOND(S)!')
+        int_sec -= 1
